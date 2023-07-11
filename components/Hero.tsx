1c425d126d3c3b8b2ff437d58cf2fd63a3c9ad59
jadashi97 <njadauniversal23@yahoo.com>
2023-07-11 06:51:50 -0500

    (feat): Hero page complete. started on About page

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,15 +3,16 @@ import React from 'react';
 import { Cursor, useTypewriter } from 'react-simple-typewriter';
 import BackgroundCircles from './BackgroundCircles';
 import Image from 'next/image';
+import Link from 'next/link';
 
 type Props = {}
 
 function Hero({ }: Props) {
     const [text, count] = useTypewriter({
         words: [
-            "~Hi, I'm Nyarji Daniel Jada~",
-            "~👨🏾‍💻A FullStack/Frontend developer~",
-            "~I focus on developing practical apps that  🎯target solving problems within my Community!~"
+            "Hi, I'm Nyarji Daniel Jada",
+            "A sports/coffee/beer lover",
+            "~I focus on developing apps that 🎯target solving problems within my Community!~"
         ],
         loop: true,
         delaySpeed: 2500,
@@ -20,16 +21,36 @@ function Hero({ }: Props) {
         <div className='h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden'>
             <BackgroundCircles />
             <Image
-                className='relative rounded-full h-38 w-36 mx-auto  object-contain'
+                className='relative rounded-full h-42 w-36 mx-auto  object-contain'
                 src="/myPic.jpeg"
                 alt={'my-pic'}
-                width={700}
+                width={800}
                 height={700}
             />
-            <h1>
-                <span>{text}</span>
-                <Cursor cursorColor='orange' />
-            </h1>
+            <div className='z-20'>
+                <h2 className='text-sm uppercase text-gray-400 pb-5 tracking-[15px]'>
+                    FullStack developer👨🏾‍💻
+                </h2>
+                <h1 className='text-3xl lg:text-4xl font-semibold px-20'>
+                    <span className='mr-3'>{text}</span>
+                    <Cursor cursorColor='orange' />
+                </h1>
+
+                <div className='pt-5'>
+                    <Link href="#about">
+                        <button className='heroButton'>About</button>
+                    </Link>
+                    <Link href="#experience">
+                        <button className='heroButton'>Experience</button>
+                    </Link>
+                    <Link href="#skills">
+                        <button className='heroButton'>Skills</button>
+                    </Link>
+                    <Link href="#projects">
+                        <button className='heroButton'>Projects</button>
+                    </Link>
+                </div>
+            </div>
         </div>
     );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 import Head from 'next/head';
 import Header from '../../components/Header';
 import Hero from '../../components/Hero';
+import About from '../../components/About';
 
 export default function Home() {
   return (
@@ -10,8 +11,11 @@ export default function Home() {
         <title>Jada's portfolio</title>
       </Head>
       <Header />
-      <section id="hero" className='snap-center'>
+      <section id="hero" className='snap-start'>
         <Hero />
+      </section>
+      <section id="about" className='snap-center'>
+        <About />
       </section>
     </div>
   )
