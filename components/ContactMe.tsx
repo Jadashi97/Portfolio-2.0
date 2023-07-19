f27c2cdd7ceb80cf0789e0a0e14e6a20f2b4dcde
jadashi97 <njadauniversal23@yahoo.com>
2023-07-18 22:10:10 -0500

    (feat): add validation form to the last page

--- components/ContactMe.tsx
+++ components/ContactMe.tsx
@@ -1,44 +1,63 @@
 /* eslint-disable react/no-unescaped-entities */
 import React from 'react';
-import { PhoneIcon, EnvelopeIcon, MapPinIcon } from '@heroicons/react/24/solid'
+import { PhoneIcon, EnvelopeIcon, MapPinIcon } from '@heroicons/react/24/solid';
+import { useForm, SubmitHandler } from "react-hook-form"
+
+type Inputs = {
+    name: string,
+    email: string,
+    subject: string,
+    message: string
+}
 
 type Props = {}
 
 export default function ContactMe({ }: Props) {
+    const {
+        register,
+        handleSubmit,
+        watch,
+        formState: { errors }
+    } = useForm<Inputs>()
+    const onSubmit: SubmitHandler<Inputs> = (formData) => console.log(formData)
     return (
         <div className='h-screen flex relative flex-col text-center md:text-left md:flex-row max-w-7xl 
         px-18 justify-evenly mx-auto items-center'>
             <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>
-                ContactMe
+                Contact Me
             </h3>
-            <div className='flex flex-col space-y-10'>
-                <h4 className='text-4xl font font-semibold text-center'>
+            <div className='flex flex-col space-y-8'>
+                <h4 className='text-4xl pt-20 font font-semibold text-center'>
                     Let's
                     <span className=' decoration-[#F7AB0A]/50 underline'> Link </span>
                     Up!
                 </h4>
-                <div className='space-y-10'>
+                <div className='space-y-8'>
                     <div className='flex items-center space-x-5 justify-center'>
                         <PhoneIcon className='text-[#F7AB0A] h-7 w-7 animate-pulse' />
-                        <p>+6124030012</p>
+                        <p className='text-2xl'>+6124030012</p>
                     </div>
                     <div className='flex items-center space-x-5 justify-center'>
                         <EnvelopeIcon className='text-[#F7AB0A] h-7 w-7 animate-pulse' />
-                        <p>lojuan247@gmail.com</p>
+                        <p className='text-2xl'>lojuan247@gmail.com</p>
                     </div>
                     <div className='flex items-center space-x-5 justify-center'>
                         <MapPinIcon className='text-[#F7AB0A] h-7 w-7 animate-pulse' />
-                        <p>Minneapolis, Minnesota</p>
+                        <p className='text-2xl'>Minneapolis, Minnesota</p>
                     </div>
                 </div>
-                <form className='flex flex-col space-y-2 w-fit mx-auto'>
+                <form
+                    onSubmit={handleSubmit(onSubmit)}
+                    className='flex flex-col space-y-2 w-fit mx-auto'>
                     <div className='flex space-x-2'>
-                        <input className='contactInput' type="text" />
-                        <input className='contactInput' type="text" />
+                        <input placeholder='Name' className='contactInput' type="text" />
+                        <input placeholder='Email' className='contactInput' type="email" />
                     </div>
-                    <input className='contactInput' type="text" />
-                    <textarea className='contactInput' />
-                    <button className='bg-[#F7AB0A] py-5 px-10 rounded-md text-black font-bold text-lg'>
+                    <input placeholder='Subject' className='contactInput' type="text" />
+                    <textarea placeholder='Message' className='contactInput' />
+                    <button
+                        type='submit'
+                        className='bg-[#F7AB0A] py-5 px-10 rounded-md text-black font-bold text-lg'>
                         Submit
                     </button>
                 </form>
