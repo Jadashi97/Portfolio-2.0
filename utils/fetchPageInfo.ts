9667cfec05635b4ac6872ea1daa7ab3eb8477239
jadashi97 <njadauniversal23@yahoo.com>
2023-08-09 17:52:50 -0500

    (feat): finished final touches and deploy site

--- utils/fetchPageInfo.ts
+++ utils/fetchPageInfo.ts
@@ -4,14 +4,14 @@ export const fetchPageInfo = async () => {
   const res = await fetch(
     `${process.env.NEXT_PUBLIC_BASE_URL}/api/getPageInfo`
   );
-
+  
+  const data = await res.json();
+  const pageInfo: PageInfo = data.pageInfo;
   // check res for error
   if (!res.ok) {
     console.log(res.statusText);
   }
 
-  const data = await res.json();
-  const pageInfo: PageInfo = data.pageInfo;
 
     // console.log("fetching", pageInfo);
 
