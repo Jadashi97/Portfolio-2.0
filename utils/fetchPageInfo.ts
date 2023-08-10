133a691bf7c855050a0f4253c3810f94c50d594a
jadashi97 <njadauniversal23@yahoo.com>
2023-08-10 06:15:00 -0500

    (fix): moved api back to pages

--- utils/fetchPageInfo.ts
+++ utils/fetchPageInfo.ts
@@ -2,16 +2,17 @@ import { PageInfo } from "../typings";
 
 export const fetchPageInfo = async () => {
   const res = await fetch(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/api/getPageInfo`
+    'http://localhost:3000/api/getPageInfo'
+    // `${process.env.NEXT_PUBLIC_BASE_URL}/api/getPageInfo`
   );
   
-  const data = await res.json();
-  const pageInfo: PageInfo = data.pageInfo;
-  // check res for error
+  // // check res for error
   if (!res.ok) {
     console.log(res.statusText);
   }
-
+  
+  const data = await res.json();
+  const pageInfo: PageInfo = data.pageInfo;
 
     console.log("fetching", pageInfo);
 
